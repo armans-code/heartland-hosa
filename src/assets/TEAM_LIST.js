d182e7aae6eac5abe73359002ac5bca50194c0e1
Arman <arman.kumaraswamy@gmail.com>
2023-10-08 15:07:56 -0400

    Update team member list, add prettier

---
+++ src/assets/TEAM_LIST.js
@@ -0,0 +1,76 @@
+import sydney from './images/portraits/sydney.png';
+import arman from './images/portraits/arman.png';
+import rishvitha from './images/portraits/rishvitha.png';
+import amrita from './images/portraits/amrita.png';
+import brooke from './images/portraits/brooke.png';
+import michael from './images/portraits/michael.png';
+import joel from './images/portraits/joel.png';
+import zarah from './images/portraits/zarah.png';
+import mars from './images/portraits/mars.png';
+
+export const TEAM_MEMBERS = [
+    {
+        fullName: 'Rayne Crenshaw',
+        title: 'President',
+        image: sydney,
+        description:
+            "Hello Heartland! My name is Rayne Crenshaw, and I am honored to be serving as your Regional President this year. I am a senior in the International Baccalaureate program at Land O' Lakes High School. When not drowning in homework, I enjoy playing soccer, reading (currently What's Not to Love), and listening to music. My favorite artists are The Weeknd and Conan Gray! I recently completed an internship in the Integrated Mathematical Oncology department of Moffitt Cancer Center over the summer. I also regularly volunteer at Special Olympics events, started a mental health awareness organization called Speaking of Stigma, and co-founded the Medical Education Club at my school. During my last year on the board, I aim to share my knowledge and resources so all of you can gain more opportunities in the medical field. I can't wait to help you all find your passions through HOSA and make lifelong memories and connections like I have!",
+    },
+    {
+        fullName: 'Arman Kumaraswamy',
+        title: 'Vice President',
+        image: arman,
+        description:
+            "Hi everyone, my name is Arman and I'lI be serving as your Vice President for the 2023-2024 year. I love mixing the healthcare field with computer science and artificial intelligence, and care deeply about making HOSA a better opportunity for all of us. As your Vice President, I strive to provide support and opportunities to illuminate your paths towards successful careers in healthcare, just like HOSA has done for me.",
+    },
+    {
+        fullName: 'Rishvitha Arikatla',
+        title: 'President-Elect',
+        image: rishvitha,
+        description:
+            "Hey Heartland HOSA! My name is Rishvitha Arikatla and I'm thrilled to be serving as your President Elect for the 2023-2024 year! I am a junior in the IB program at Land O' Lakes high school and some of my favorite things include dancing, reading, and meeting new people. My experiences have shown me time and time again how HOSA is so incredible so I want to be able to share these experiences with you all! This year I aim to foster a strong environment for growth and learning so members can reach their full potential in the healthcare field. I'm very excited for a great year ahead!",
+    },
+    {
+        fullName: 'Amrita Vaka',
+        title: 'Vice President-Elect',
+        image: amrita,
+        description:
+            "Hello Heartland Region! My name is Amrita Vaka, and I am honored to serve as your Vice President Elect for 2023-2024. I am a junior in the medical academy at Wiregrass Ranch High School. I enjoy listening to music, baking, playing the violin, and drawing. I am so excited to bring a fresh perspective to the table with my background in other leadership positions for a variety of clubs at my school. I love that HOSA has helped me show how passionate I am about healthcare, and I hope that all HOSA members can one day feel the same. I am committed to making sure that we all can achieve the goals and missions of HOSA. I can't wait to work with you guys to make many lifelong memories!",
+    },
+    {
+        fullName: 'Brooke Carpio',
+        title: 'Parliamentarian',
+        image: brooke,
+        description:
+            "Hi Heartland HOSA! My name is Brooke Carpio, I'm a senior in the IB Programme at Land O Lakes High School and I'm so excited to serve as your parliamentarian for 2023-2024. When I'm not studying or shadowing at Tampa General Hospital, I'm cheerleading on the varsity team for my high school. I also love music and dance (current favorite artists are LESSERAFIM, NewJeans, and Chase Atlantic). This year I hope to make the members of Heartland feel more connected and included, so we can all get to know each other and learn from fellow HOSA members across the region. I'm extremely passionate about medicine, and the importance of this field of study in our society. I want for all of Heartland to feel the thrill of finding their passion through competition, events, and meeting with this incredible community of future health professionals. I'm extremely grateful to be serving you all, and I'm looking forward to an incredible year!",
+    },
+    {
+        fullName: 'Michael Vento',
+        title: 'Reporter',
+        image: michael,
+        description:
+            "Hello, Heartland! I'm Michael Vento, your enthusiastic Reporter for 2023-2024. With a passion for storytelling, my mission is to share the remarkable achievements of our HOSA chapter with the world. I'm a clarinet player, avid reader, and dedicated volunteer at Advent Health, where I witness firsthand the power of compassion in healthcare. I also am a Advocacy + Inclusion Specialist for #ICANHELP and a cofounder of Medical Education Club. I'm committed to empowering our members and fostering a thriving community while also having fun. Together, let's make a lasting impact and inspire others through the incredible journey of Heartland HOSA!",
+    },
+    {
+        fullName: 'Joel Johnson',
+        title: 'Historian',
+        image: joel,
+        description:
+            "Hi my name Joel Johnson and Im a Senior at Winter Haven High School. I will be the Heartland Region's Historian for the 2023-2024 School Year. Besides HOSA i'm also involved in FFA. When i'm not doing school or driving everyone everywhere(safely) I'm practicing the piano or talking and hanging out with my friends. Fair warning i have a loud personality and hope to use it to get you excited for HOSA and all it has to offer. Thank you for the Opportunity to serve you all and can't wait to meet you all!!",
+    },
+    {
+        fullName: 'Tanvi Handoo',
+        title: 'Secretary',
+        image: zarah,
+        description:
+            "Hello Heartland HOSA! My name is Zarah Kerawala and this year I will be serving as your 2023-2024 regional Secretary. I enjoy trying new experiences, learning new things, and meeting new people! I hope to serve this year by creating a great environment for Heartland, one that is fun, but also one that raises awareness for the healthcare field. One of the best experiences I have had while being a member was my first ILC. I can't wait to guide you all to find your spark in the field and make connections that last forever!",
+    },
+    {
+        fullName: 'Mars McDermott',
+        title: 'Treasurer',
+        image: mars,
+        instagramLink: 'https://www.instagram.com/sarvesh.gade123/',
+        description:
+            "Hi everybody, my name is Mars McDermott and I'm a sophomore at Winter Haven High School. I will be your Treasurer for the Heartland Region in 23-24. I love spending time with my friends, hanging out with my cats and I absolutely love theater. I feel very strongly about advocating for mental health and help an organization called NAMI as much as possible. I want to serve all the Heartland Region to the highest of standards and ultimately help everyone work towards their medical career dreams. I can't wait to continue making memories with you all! thank you for letting me serve you Heartland HOSA.",
+    },
+];
